c3c1eb788adfca918a639b64c60ad942a1685a08
El3ctr1cR <ruben.ruitenberg@hotmail.com>
2026-02-20 11:21:42 +0100

    Added automated Datto RMM deployment option

---
+++ DattoRMM/modules/core/Connect-MgGraph.psm1
@@ -0,0 +1,32 @@
+﻿$GraphScopes = @(
+    "DeviceManagementConfiguration.ReadWrite.All",
+    "DeviceManagementApps.ReadWrite.All",
+    "Group.ReadWrite.All",
+    "User.ReadWrite.All",
+    "Policy.ReadWrite.AuthenticationMethod",
+    "Policy.ReadWrite.DeviceConfiguration",
+    "DeviceManagementManagedDevices.ReadWrite.All",
+    "Directory.ReadWrite.All",
+    "DeviceManagementServiceConfig.ReadWrite.All",
+    "Device.ReadWrite.All",
+    "WindowsUpdates.ReadWrite.All"
+)
+
+function Connect-ToMSGraph {
+    try {
+        Disconnect-MgGraph -ErrorAction SilentlyContinue
+        Write-Host "Connecting to Microsoft Graph..." -ForegroundColor Yellow
+
+        # Force interactive login, no cached session persistence
+        Connect-MgGraph -Scopes $GraphScopes -NoWelcome -ContextScope Process -ErrorAction Stop
+
+        Write-Host "Successfully connected to Microsoft Graph" -ForegroundColor Green
+        return $true
+    }
+    catch {
+        Write-Error "Failed to connect to Microsoft Graph: $($_.Exception.Message)"
+        return $false
+    }
+}
+
+Export-ModuleMember -Function Connect-ToMSGraph
